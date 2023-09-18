acf85e60b41d509d91d28ea4c6b29cb67b8010bc
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-09-18 12:36:06 +0200

    .NET Essentials 18/09/2023

---
+++ BA2/DotNet-Essentials/First-Console-App/First-Console-App/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FirstConsoleApp;
+internal class Program
+{
+    static void Main(string[] _)
+    {
+        Console.WriteLine("Hello, what is your name?");
+        string? name = Console.ReadLine();
+        if (name == null) {
+            Console.WriteLine("Why you null??");
+        } else
+        {
+            Console.WriteLine($"Hello, {name}. Here is a sum for you! {Sum(5, 1)}");
+        }
+
+        // Keep console open until enter is pressed - visual studio is dumb
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Sum of two ints - addition is communitive
+    /// </summary>
+    /// <param name="a">First</param>
+    /// <param name="b">Second</param>
+    /// <returns>Addition</returns>
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
+
