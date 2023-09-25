359002ba7a024fa54ec6b6643c1d7751b3cea52c
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-09-25 14:01:36 +0200

    Demo with nullable

---
+++ BA2/DotNet-Essentials/demo/MyConsoleDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MyConsoleDemo
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Shorted turnary operator of value != null ? value : default
+            // Pretty cool
+            string name = Console.ReadLine() ?? "Unkown";
+
+            Console.WriteLine($"Hello {name.ToUpper()}!");
+        }
+    }
+}
