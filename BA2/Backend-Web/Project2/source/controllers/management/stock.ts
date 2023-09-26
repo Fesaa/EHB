b88462846838fa3051bd9958ae8dfd40f99bf6f7
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-09-27 00:23:12 +0200

    Move to different sqlite lib & General design idea

--- BA2/Backend-Web/Project2/source/controllers/management/stock.ts
+++ BA2/Backend-Web/Project2/source/controllers/management/stock.ts
@@ -1,18 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import { insertItem } from "../../db";
+import { databaseManager } from "../../database/DatabaseManager";
 
 async function addItemToStock(req: Request, res: Response, next: NextFunction) {
     let name: string = req.body.name
-    let price: Number = req.body.price
-    let amount: Number = req.body.amount
+    let price: number = req.body.price
+    let amount: number = req.body.amount
 
-    insertItem({
+
+    const ids = await databaseManager.addNewItems([{
         name: name,
         price: price,
-        amount: amount,
-    })
-
-    return res.status(200).json({ msg: "OK!" })
+        amount: amount
+    }])
+    return res.status(200).json({ msg: "OK!", ids: ids })
 
 }
 
--- BA2/Backend-Web/Project2/source/server.ts
+++ BA2/Backend-Web/Project2/source/server.ts
@@ -1,34 +1,39 @@
 import http from "http";
 import morgan from "morgan";
 import express, { Express } from "express";
-import routes from "./routes/orders"
+import routes from "./routes";
+import { databaseManager } from "./database/DatabaseManager";
 
-const router: Express = express();
+async function main() {
 
-// Loggings
-router.use(morgan("dev"));
-router.use(express.urlencoded({ extended: false }));
-router.use(express.json());
-router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
-        return res.status(200).json({});
-    }
-    next();
-});
+    await databaseManager.init()
 
-router.use("/", routes)
+    const router: Express = express();
 
-router.use((_, res, __) => {
-    return res.status(404).json({
-        message: 'Endpoint was not found.'
+    // Loggings
+    router.use(morgan("dev"));
+    router.use(express.urlencoded({ extended: false }));
+    router.use(express.json());
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+            return res.status(200).json({});
+        }
+        next();
     });
-});
 
-const server = http.createServer(router);
-const PORT = process.env.PORT ?? 6060;
-server.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+    router.use("/", routes)
 
+    router.use((_, res, __) => {
+        return res.status(404).json({
+            message: 'Endpoint was not found.'
+        });
+    });
+    const server = http.createServer(router);
+    const PORT = process.env.PORT ?? 6060;
+    server.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+}
 
+main()
