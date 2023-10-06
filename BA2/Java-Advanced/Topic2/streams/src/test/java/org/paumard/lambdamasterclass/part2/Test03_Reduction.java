5d94576294569508d7c151829c31fef0c011f648
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-10-07 01:27:34 +0200

    Topic2

---
+++ BA2/Java-Advanced/Topic2/streams/src/test/java/org/paumard/lambdamasterclass/part2/Test03_Reduction.java
@@ -0,0 +1,20 @@
+package org.paumard.lambdamasterclass.part2;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test03_Reduction {
+
+    @Test
+    public void reduction_1() {
+        long number = 21;
+
+        // Not enough info lol?
+        BigInteger result = null; // TODO
+
+        assertThat(result).isEqualTo(new BigInteger("51090942171709440000"));
+    }
+}
