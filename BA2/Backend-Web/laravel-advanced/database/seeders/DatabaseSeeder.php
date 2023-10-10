3f198d6c22a4cb3b76dce6b0ef64f5bbf21c53a7
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-10-10 03:25:17 +0200

    - Better seed - Fix comment owner

--- BA2/Backend-Web/laravel-advanced/database/seeders/DatabaseSeeder.php
+++ BA2/Backend-Web/laravel-advanced/database/seeders/DatabaseSeeder.php
@@ -16,34 +16,66 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        $user = User::factory()->create([
-            'name' => 'TestUser',
-            'email' => 'test2@example.com',
+        $sarah = User::factory()->create([
+            'name' => 'Sarah',
+            'email' => 'sarah@example.com',
             'password' => bcrypt('password'), ]);
 
+        $michal = User::factory()->create([
+            'name' => 'Michal',
+            'email' => 'michal@example.com',
+            'password' => bcrypt('password'), ]);
+
+
+        $emily = User::factory()->create([
+            'name' => 'Emily',
+            'email' => 'emily@example.com',
+            'password' => bcrypt('password'), ]);
+
+
+        $james = User::factory()->create([
+            'name' => 'James',
+            'email' => 'james@example.com',
+            'password' => bcrypt('password'), ]);
+
+
+        $rebecca = User::factory()->create([
+            'name' => 'Rebecca',
+            'email' => 'rebecca@example.com',
+            'password' => bcrypt('password'), ]);
+
+
         $post1 = Post::factory()->create([
-            'title' => 'Test Post',
-            'content' => 'This is a test post',
-            'user_id' => $user->id,
+            'title' => 'Thanks for hosting!',
+            'content' => 'What a lovely event! Thanks for having us. The food was delicious, and the decorations were beautiful. Cant wait for the next gathering!',
+            'user_id' => $sarah->id,
         ]);
 
         $post2 = Post::factory()->create([
-            'title' => 'Test Post 2',
-            'content' => 'This is a test post 2',
-            'user_id' => $user->id,
+            'title' => 'Had a lot of fun!',
+            'content' => 'It was a pleasure being here today. The music was fantastic, and I had a great time catching up with old friends. Thanks for the wonderful hospitality!',
+            'user_id' => $michal->id,
         ]);
 
         Comment::factory()->create([
-            'content' => 'This is a test comment',
-            'user_id' => $user->id,
+            'content' => 'Michael, it was so nice seeing you too! Let us not wait so long before the next get-together. 😊',
+            'user_id' => $emily->id,
+            'post_id' => $post2->id,
+        ]);
+
+        Comment::factory()->create([
+            'content' => 'This was truly a memorable occasion. The speeches were heartfelt, and the atmosphere was warm and welcoming. Thanks for including us in your celebration!',
+            'user_id' => $james->id,
+            'post_id' => $post2->id,
+        ]);
+
+        Comment::factory()->create([
+            'content' => 'Thanks for the kind words, Sarah. It was a pleasure having you and your family here. We hope to see you again soon!',
+            'user_id' => $rebecca->id,
             'post_id' => $post1->id,
         ]);
 
-        Comment::factory()->create([
-            'content' => 'This is a test comment 2',
-            'user_id' => $user->id,
-            'post_id' => $post2->id,
-        ]);
+
 
     }
 }
--- BA2/Backend-Web/laravel-advanced/resources/views/content/comment.blade.php
+++ BA2/Backend-Web/laravel-advanced/resources/views/content/comment.blade.php
@@ -1,7 +1,7 @@
 
 <div class="comment">
     <div class="post-top">
-        <h3 class="post-title">By {{ $post->user->name }}</h3>
+        <h3 class="post-title">By {{ $comment->user->name }}</h3>
         <p class="creation-date">{{ $comment->created_at }}</p>
     </div>
     <p class="post-content">{{ $comment->content }}</p>
