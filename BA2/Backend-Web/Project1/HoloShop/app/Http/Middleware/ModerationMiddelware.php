234489418527d53a902e757fe7a21e94421de049
Amelia <77553571+Fesaa@users.noreply.github.com>
2023-10-12 22:22:47 +0200

    Add bans

---
+++ BA2/Backend-Web/Project1/HoloShop/app/Http/Middleware/ModerationMiddelware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Privilege;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ModerationMiddelware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->url() == route('ban')) {
+            return $next($request);
+        }
+        if (auth()->user() != null) {
+            if (auth()->user()->hasPrivilege(Privilege::getPrivilegeValue('NOT_GLOBAL_SITE'))) {
+                return redirect()->route('ban');
+            }
+        }
+
+        return $next($request);
+    }
+}
