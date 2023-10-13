8edc8f49bcc06037aa51edc56485ab5550da9b60
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-10-13 13:13:00 +0200

    Hide some floats if empty

--- BA2/Backend-Web/Project1/HoloShop/resources/views/partials/infobar.blade.php
+++ BA2/Backend-Web/Project1/HoloShop/resources/views/partials/infobar.blade.php
@@ -8,36 +8,47 @@
         </div>
     </div>
 
-    <div class="float">
-        <div class="infobar-title">Online Staff</div>
-        <div class="infobar-content" style="padding: 0 !important;">
-            <ul class="infobar-staff-list" >
-                @foreach(\App\Models\Activity::staffOnlineInLast(10) as $activity)
-                    <li>@include('objects.mini_profile', ['user' => $activity->user()->first()])</li>
-                @endforeach
-            </ul>
+    @php($onlineStaff = \App\Models\Activity::staffOnlineInLast(10))
+    @if(sizeof($onlineStaff) > 0)
+        <div class="float">
+            <div class="infobar-title">Online Staff</div>
+            <div class="infobar-content" style="padding: 0 !important;">
+                <ul class="infobar-staff-list" >
+                    @foreach($onlineStaff as $activity)
+                        <li>@include('objects.mini_profile', ['user' => $activity->user()->first()])</li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
+    @endif
 
     <div class="float">
         <div class="infobar-title">Online members</div>
         <div class="infobar-content">
             <ul class="flat-list">
-                @foreach(\App\Models\Activity::onlineInLast(10) as $activity)
-                    <li><a class="clean-link" href="{{ route('profile.show', $activity->user_id) }}">{!! $activity->getName() !!}</a></li>
-                @endforeach
+                @php($online = \App\Models\Activity::onlineInLast(10))
+                @if(sizeof($online) > 0)
+                    @foreach($online as $activity)
+                        <li><a class="clean-link" href="{{ route('profile.show', $activity->user_id) }}">{!! $activity->getName() !!}</a></li>
+                    @endforeach
+                @else
+                    <li>...</li>
+                @endif
             </ul>
         </div>
     </div>
 
-    <div class="float">
-        <div class="infobar-title">Birthdays</div>
-        <div class="infobar-content">
-            <ul class="flat-list">
-                @foreach(\App\Http\Controllers\UserController::getTodaysBirthDays() as $user)
-                    <li><a class="clean-link" href="{{ route('profile.show', $user->id) }}">{!! $user->getColouredName() !!}</a></li>
-                @endforeach
-            </ul>
+    @php($birthdays = \App\Http\Controllers\UserController::getTodaysBirthDays())
+    @if(sizeof($birthdays) > 0)
+        <div class="float">
+            <div class="infobar-title">Birthdays</div>
+            <div class="infobar-content">
+                <ul class="flat-list">
+                    @foreach(\App\Http\Controllers\UserController::getTodaysBirthDays() as $user)
+                        <li><a class="clean-link" href="{{ route('profile.show', $user->id) }}">{!! $user->getColouredName() !!}</a></li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
+    @endif
 </div>
