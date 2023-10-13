54af70f5500b50e9cea6afd479acabae4a60ca53
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-10-14 01:51:24 +0200

    About me's

--- BA2/Backend-Web/Project1/HoloShop/database/seeders/SetupDefaultAccounts.php
+++ BA2/Backend-Web/Project1/HoloShop/database/seeders/SetupDefaultAccounts.php
@@ -46,14 +46,14 @@ class SetupDefaultAccounts extends Seeder
         $ehb_account->roles()->attach($STAFF);
 
         $funNames = [
-            'Luna Starlight',
-            'Captain Crunch',
-            'Ruby Redd',
-            'Galaxy Explorer',
-            'Max Thunderbolt'
+            'Luna Starlight' => "Hey there, I'm Luna Starlight, and I'm absolutely starry-eyed about the night sky! When I'm not chatting with you all, I'm wrapped up in my cozy blanket, sipping moonlight tea, and gazing at the stars. You can often find me lost in thought, penning poetry inspired by the cosmos. Let's journey through the universe together, one twinkling star at a time!",
+            'Captain Crunch' => "Ahoy, me hearties! I'm Captain Crunch, a true seafarer at heart. When I'm not sharing tales of nautical adventures, I'm out there plundering buried treasure and exploring the seven seas. Join me for some swashbuckling stories and salty sea dog banter. Bring your spyglass and let's set sail together!",
+            'Ruby Redd' => "Hey, I'm Ruby Redd, your resident dancing diva! When I'm not shaking a leg or dazzling the digital dance floor, I'm here to spill the tea on style, makeup, and all things fabulous. Get ready to groove with me and embrace the glam life. Let's make every day a runway!",
+            'Galaxy Explorer' => "Greetings, fellow space enthusiasts! I'm Galaxy Explorer, and my passion is uncovering the mysteries of the cosmos. With my trusty telescope in hand, I'll take you on interstellar journeys through black holes, alien encounters, and everything in between. So, fasten your seatbelts, because we're in for an epic starship ride to remember!",
+            'Max Thunderbolt' => "What's up, thrill-seekers? I'm Max Thunderbolt, and I live for the adrenaline rush. When I'm not sharing my daring escapades and extreme sports experiences, you'll find me on the edge of excitement. From skydiving to storm-chasing, it's all about that electrifying thrill. So, buckle up and get ready for a jolt of excitement!",
         ];
 
-        foreach ($funNames as $name) {
+        foreach ($funNames as $name => $bio) {
             $email = strtolower(str_replace(' ', '.', $name)) . '@example.com';
             $password = bcrypt('password');
 
@@ -64,6 +64,10 @@ class SetupDefaultAccounts extends Seeder
             ]);
 
             $user->roles()->attach($MEMBER);
+
+            $profile = $user->getProfile();
+            $profile->bio = $bio;
+            $profile->save();
         }
     }
 }
--- BA2/Backend-Web/Project1/HoloShop/database/seeders/SetupDefaultProfiles.php
+++ BA2/Backend-Web/Project1/HoloShop/database/seeders/SetupDefaultProfiles.php
@@ -20,16 +20,19 @@ class SetupDefaultProfiles extends Seeder
         $profile->title = 'Founder';
         $profile->pfp_asset_id = 1;
         $profile->banner_asset_id = 4;
+        $profile->bio = "Hey there, fabulous forum fanatics! 🌟 I'm the quirky queen bee behind this online hangout, and let me tell you, it's all about bringing the sparkle to your digital life! 👑✨ When I'm not dreaming up emojis or sipping on unicorn lattes (extra glitter, please), you can find me navigating the wacky world of internet with the same passion and zeal as a puppy chasing its tail. I believe that our forum is more than just a place to chat; it's a vibrant, virtual garden where friendships bloom and ideas take flight. So, grab your coffee, throw on your favorite emoji pajamas, and let's spread the love and laughter, one post at a time! 💬💕";
         $profile->save();
 
         $profile = $moderator->getProfile();
         $profile->title = 'Moderator';
         $profile->pfp_asset_id = 2;
+        $profile->bio = "Hello, fabulous forum fam! ✨I'm the volunteer moderator with a sprinkle of glitter and a whole lot of heart! 🌟🦄 I might not have a superhero cape, but I'm here to save the day (and your sanity) by keeping our forum as bubbly and positive as a champagne toast at a unicorn-themed birthday party. When I'm not chasing rainbows or perfecting my emoji game, I'm on a mission to make sure this forum is a place where every member feels heard and loved. 🌈💬 So, whether you need help, a friendly chat, or just a virtual hug, I'm your gal! Let's make this digital world a little more magical, one sparkly post at a time. 😄💖";
         $profile->save();
 
         $profile = $ehb->getProfile();
         $profile->pfp_asset_id = 3;
         $profile->banner_asset_id = 5;
+        $profile->bio = "Greetings to the community, I'm the administrator and caretaker of our cherished forum, and it's a privilege to be entrusted with the responsibility of maintaining this thriving digital space. While I might not be donning a cape, I do consider myself a guardian of order and civility in our online home. My goal is to ensure that our forum remains a respectful and informative environment, where ideas can be shared, questions can be answered, and camaraderie can flourish. I'm dedicated to upholding the rules, preserving the quality of discussions, and providing the support needed to keep this community strong. If you ever have questions or concerns, don't hesitate to reach out. Let's work together to make this forum the best it can be.";
         $profile->save();
 
     }
