b8e41ef17dbaf3826ee20b5a5d96df67f1f21bb7
Amelia <77553571+Fesaa@users.noreply.github.com>
2023-10-17 18:45:32 +0200

    Re-add footer

--- BA2/Backend-Web/Project1/HoloLand/resources/views/layouts/master.blade.php
+++ BA2/Backend-Web/Project1/HoloLand/resources/views/layouts/master.blade.php
@@ -1,53 +1,57 @@
 <link href="{{ asset('css/shared/global.css') }}" rel="stylesheet">
 <link href="{{ asset('css/layouts/master.css') }}" rel="stylesheet">
 
-@include('partials.header')
+<div id="body">
+    @include('partials.header')
 
-<div class="master-formatting">
+    <div class="master-formatting">
 
-    <div class="master-formatting-sidebar">
-        @include('partials.sidebar')
-    </div>
+        <div class="master-formatting-sidebar">
+            @include('partials.sidebar')
+        </div>
 
-    <div class="master-formatting-main-content">
-        @yield('main-content')
-    </div>
+        <div class="master-formatting-main-content">
+            @yield('main-content')
+        </div>
 
-    <div class="master-formatting-infobar">
-        @include('partials.infobar')
-    </div>
-</div>
-
-@if($errors->any())
-<div id="error-popup-container" class="flex-column" style="display: flex !important;">
-    <div id="error-popup" class="flex-row" style="justify-content: center">
-        <div class="errors flex-column">
-            <p>@yield('errors-title')</p>
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li class="error">{{ $error }}</li>
-                @endforeach
-            </ul>
-            <div onclick="closePopup()" id="error-close">Close</div>
+        <div class="master-formatting-infobar">
+            @include('partials.infobar')
         </div>
     </div>
-</div>
-<script>
-    let errorPopupContainer = document.getElementById("error-popup-container");
-    let errorPopup = document.getElementById("error-popup");
-    [errorPopupContainer, errorPopup].forEach((e) => {
-        e.addEventListener('click', function (event) {
-            if (event.target === e) {
+
+    @if($errors->any())
+        <div id="error-popup-container" class="flex-column" style="display: flex !important;">
+            <div id="error-popup" class="flex-row" style="justify-content: center">
+                <div class="errors flex-column">
+                    <p>@yield('errors-title')</p>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="error">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    <div onclick="closePopup()" id="error-close">Close</div>
+                </div>
+            </div>
+        </div>
+        <script>
+            let errorPopupContainer = document.getElementById("error-popup-container");
+            let errorPopup = document.getElementById("error-popup");
+            [errorPopupContainer, errorPopup].forEach((e) => {
+                e.addEventListener('click', function (event) {
+                    if (event.target === e) {
+                        errorPopupContainer.style.display = "none";
+                    }
+                });
+            });
+
+            function closePopup() {
                 errorPopupContainer.style.display = "none";
             }
-        });
-    });
 
-    function closePopup() {
-        errorPopupContainer.style.display = "none";
-    }
+        </script>
+    @endif
+</div>
 
-</script>
-@endif
 
-<!--@include('partials.footer')-->
+
+@include('partials.footer')
--- BA2/Backend-Web/Project1/HoloLand/resources/views/partials/footer.blade.php
+++ BA2/Backend-Web/Project1/HoloLand/resources/views/partials/footer.blade.php
@@ -1,30 +1,24 @@
 <link rel="stylesheet" href="{{ asset('css/partials/footer.css') }}">
 
-<footer class="float">
+<footer class="curvetop">
 
-    <div class="footer-item flex-column">
-        <div class="footer-item">
-            <h3>My footer</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="footer-holder flex-row">
+        <div class="footer-item flex-column">
+            <div class="footer-item">
+                <h3>About me</h3>
+                <p>I'm a website made for the Backend Web 2023 Course on the EHB.
+                    With heavy layout inspiration from <a class="footer-link" href="https://www.cubecraft.net/" target="_blank">CubeCraft</a>
+                    , and the name from <a class="footer-link" href="https://spice-and-wolf.com/" target="_blank">Spice and Wolf</a>.
+                </p>
+            </div>
+            <div class="footer-item">
+                <h3>Handy Links!</h3>
+                <ul class="flat-list">
+                    <li><a class="footer-link" href="{{ route('forums.show', 4) }}">FAQ</a></li>
+                    <li><a class="footer-link" href="{{ route('threads.show', 7) }}">Rules</a></li>
+                    <li><a class="footer-link" href="{{ route('forums.show', 5) }}">Support</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="footer-item">
-            <h3>Support</h3>
-            <ul class="flat-list">
-                <li>FAQ</li>
-                <li>Help</li>
-                <li>Support</li>
-            </ul>
-        </div>
-    </div>
-
-    <div class="footer-item">
-        <p>Connect with me!</p>
-        <ul>
-            <li>Facebook</li>
-            <li>Twitter</li>
-            <li>Instagram</li>
-            <li>Discord</li>
-            <li>MyAnimeList</li>
-        </ul>
     </div>
 </footer>
