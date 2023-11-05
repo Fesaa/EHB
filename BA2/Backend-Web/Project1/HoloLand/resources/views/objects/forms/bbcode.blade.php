4f8b109e0d42de18994eddc5b9865801cd639143
Amelia <77553571+Fesaa@users.noreply.github.com>
2023-11-05 13:02:54 +0100

    Typo

--- BA2/Backend-Web/Project1/HoloLand/resources/views/objects/forms/bbcode.blade.php
+++ BA2/Backend-Web/Project1/HoloLand/resources/views/objects/forms/bbcode.blade.php
@@ -1,5 +1,5 @@
 <label for="{{ $type }}"> {{$label}} </label>
 <p style="font-size: small; text-align: left">
-    BBCode is avaible for formatting, use square brackets with specific tags like [b] for bold text or [url] for hyperlinks. For example, [b]This is bold[/b] and [url=https://www.example.com]Visit here[/url]. If you want to quote text, use [quote]. For further details, check this <a target="_blank" href="https://en.wikipedia.org/wiki/BBCode">page</a>.
+    BBCode is available for formatting, use square brackets with specific tags like [b] for bold text or [url] for hyperlinks. For example, [b]This is bold[/b] and [url=https://www.example.com]Visit here[/url]. If you want to quote text, use [quote]. For further details, check this <a target="_blank" href="https://en.wikipedia.org/wiki/BBCode">page</a>.
 </p>
 <textarea id="{{ $type }}" name="{{ $type }}" cols="100" rows="15">{{ $value }}</textarea><br>
