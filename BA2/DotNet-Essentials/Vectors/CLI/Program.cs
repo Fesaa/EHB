4f8aaa27e905ab135971b5aaa296b3d8d439f637
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-11-27 16:08:30 +0100

    Oefening 1-2

---
+++ BA2/DotNet-Essentials/Vectors/CLI/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using Vector;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            VectorImpl<int> v = new VectorImpl<int>();
+
+            v.PushBack(12);
+            v.PushBack(13);
+            v.PushBack(11);
+            v.PushBack(19);
+            v.PopBack();
+
+            Console.WriteLine(v.ToString());
+
+            foreach (int i in v)
+            {
+                Console.WriteLine(i);
+            }
+
+            VectorImpl<Email> emails = new VectorImpl<Email>();
+
+            List<string> to = new List<string>();
+            to.Add("test1@email");
+            to.Add("test2@email");
+
+            emails.PushBack(new Email("Hello, World!", "Hello, World!", "example@email", to, DateTime.Now.AddDays(-1)));
+            emails.PushBack(new Email("Hello, World 1!", "Hello, World!", "example2@email", to, DateTime.Now.AddHours(-1)));
+            emails.PushBack(new Email("Hello, World 2!", "Hello, World!", "example3@email", to, DateTime.Now));
+
+
+            emails.Where(e => e.Datum.Date == DateTime.Now.Date).OrderByDescending(e => e.Datum).ToList().ForEach(e => Console.WriteLine(e.ToString()));
+            emails.Where(e => e.Datum.Year == DateTime.Now.Year).OrderByDescending(e => e.Datum).ToList().ForEach(e => Console.WriteLine(e.ToString()));
+
+            string zoekTerm = Console.ReadLine();
+            if (zoekTerm == null) return;
+
+            emails.Where(e => e.Inhoud.Contains(zoekTerm)).OrderByDescending(e => e.Datum).ToList().ForEach(e => Console.WriteLine(e.ToString()));
+            Console.WriteLine(emails.Where(e => ISOWeek.GetWeekOfYear(e.Datum) == ISOWeek.GetWeekOfYear(DateTime.Now)).OrderByDescending(e => e.Datum).Count());
+            Console.WriteLine(emails.OrderByDescending(e => e.Datum).First().ToString());
+
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            Console.WriteLine("Grootste 3: " + String.Join(", ", numbers.OrderBy(n => n).TakeLast(3)));
+            Console.WriteLine("Som: " + numbers.Sum());
+            Console.WriteLine("Gemiddelde: " + numbers.Average());
+            Console.WriteLine("Even getallen: " + String.Join(", ", numbers.Where(n => n % 2 == 0)));
+        }
+    }
+}
---
+++ BA2/DotNet-Essentials/Vectors/Vector/Vector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Vector
+{
+    public class VectorImpl<T> : IEnumerable<T>
+    {
+
+        private T[] _items;
+
+        private int count;
+
+        public VectorImpl() : this(1) { }
+
+        public VectorImpl(int initialCapacity)
+        {
+            _items = new T[initialCapacity];
+            count = 0;
+        }
+
+        public void PushBack(T item)
+        {
+            if (_items.Length == count)
+                Grow();
+            _items[count++] = item;
+        }
+
+        public T PopBack()
+        {
+            if (count == 0)
+            {
+                return default(T);
+            }
+            return _items[--count];
+        }
+
+        public T GetItem(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return _items[index];
+        }
+
+        public override String ToString()
+        {
+            return String.Join(" ", _items.Take(count));
+        }
+
+        private void Grow()
+        {
+            T[] newItems = new T[_items.Length * 2];
+            Array.Copy(_items, newItems, _items.Length);
+            _items = newItems;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
