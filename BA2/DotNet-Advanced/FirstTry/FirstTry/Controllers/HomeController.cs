8ac751a5b29cb81da089f37c32c9d182ecd9bba9
Fesaa <77553571+Fesaa@users.noreply.github.com>
2024-02-15 10:38:23 +0100

    Playing around

--- BA2/DotNet-Advanced/FirstTry/FirstTry/Controllers/HomeController.cs
+++ BA2/DotNet-Advanced/FirstTry/FirstTry/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public string About()
+    {
+        return "Hello, World!";
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
