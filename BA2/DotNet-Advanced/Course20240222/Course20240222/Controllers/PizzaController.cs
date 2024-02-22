b0f5daa864f5bf7410921ddfaa438072342bda43
Fesaa <77553571+Fesaa@users.noreply.github.com>
2024-02-22 11:41:40 +0100

    Oefening 4

---
+++ BA2/DotNet-Advanced/Course20240222/Course20240222/Controllers/PizzaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Course20240222.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Course20240222.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        public IActionResult Bestellen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Bestel([FromForm]PizzaModel pizzaModel)
+        {
+            if (!pizzaModel.IsValid())
+            {
+                return NotFound("You bitch!");
+            }
+            return View("Toon", pizzaModel);
+        }
+    }
+}
+
---
+++ BA2/DotNet-Advanced/Course20240222/Course20240222/Models/PizzaModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Course20240222.Models
+{
+	public class PizzaModel
+	{
+
+		public string Besteller { get; set; } = string.Empty;
+		public string Pizza { get; set; } = string.Empty;
+		public bool ExtraKaas { get; set; }
+		public BetaalWijze BetaalWijze { get; set; }
+
+
+		public bool IsValid()
+		{
+			return !(string.IsNullOrEmpty(Besteller)
+				|| string.IsNullOrEmpty(Pizza));
+		}
+	}
+
+
+	public enum BetaalWijze
+	{
+		[Display(Name = "Cash")] CASH,
+        [Display(Name = "Bankcontact")] BANKCONTACT,
+        [Display(Name = "Overschrijving")] OVERSCHRIJVING
+	}
+}
+
